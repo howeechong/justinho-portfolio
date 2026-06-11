d3340713269587e67339687dd2816aaf850f8400
Justin Ho <howeechong@hotmail.com>
2026-06-11 23:03:02 +0800

    Complete portfolio project pages

---
+++ src/data/portfolio.js
@@ -0,0 +1,25 @@
+export const portfolioProjects = [
+  {
+    title: "Fairprice Meet the Fairmily",
+    category: "Editing",
+    client: "NTUC Fairprice",
+    slug: "fairprice-meet-the-fairmily",
+    thumbnail: "/images/portfolio/fairprice.png"
+  },
+
+  {
+    title: "Sushiro All Star",
+    category: "Videography",
+    client: "Sushiro Singapore",
+    slug: "sushiro-all-star",
+    thumbnail: "/images/portfolio/sushiro.jpg"
+  },
+
+  {
+    title: "Income Comeback Workouts",
+    category: "Directing",
+    client: "Income Insurance",
+    slug: "income-comeback-workouts",
+    thumbnail: "/images/portfolio/incomecomback.png"
+  }
+];
