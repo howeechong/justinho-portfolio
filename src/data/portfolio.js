f7e48f8df13999ff1b5201b33d59b05792b2bd7b
Justin Ho <howeechong@hotmail.com>
2026-06-13 01:15:54 +0800

    Add photography galleries, about page updates and favicon

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -9,7 +9,7 @@ export const portfolioProjects = [
 
   {
     title: "Sushiro All Star",
-    category: "Videography",
+    category: "Videography & Editing",
     client: "Sushiro Singapore",
     slug: "sushiro-all-star",
     thumbnail: "/images/portfolio/sushiro.jpg"
@@ -21,5 +21,40 @@ export const portfolioProjects = [
     client: "Income Insurance",
     slug: "income-comeback-workouts",
     thumbnail: "/images/portfolio/incomecomback.png"
+  },
+
+    {
+    title: "NexGen Winners Surprise",
+    category: "Videoography & Editing",
+    client: "Association of Advertising and Marketing Singapore (AAMS)",
+    slug: "nexgen-surprise",
+    thumbnail: "/images/portfolio/nexgen-surprise.png"
+  },
+  
+    {
+    title: "Behind The Scenes",
+    category: "Photography",
+    client: "On Set",
+    slug: "behind-the-scenes",
+    link: "/photography/behind-the-scenes",
+    thumbnail: "/images/photography/behind-the-scenes/BTS_7.jpg"
+  },
+
+  {
+    title: "Galaxy Touch Rugby",
+    category: "Photography",
+    client: "Galaxy Touch Singapore",
+    slug: "touch-rugby",
+    link: "/photography/touch-rugby",
+    thumbnail: "/images/photography/touch-rugby/TR_5.jpg"
+  },
+
+  {
+    title: "Jeju Haenyeo",
+    category: "Photography",
+    client: "Cultural Documentary",
+    slug: "jeju-haenyeo",
+    link: "/photography/jeju-haenyeo",
+    thumbnail: "/images/photography/jeju-haenyeo/JEJU_1.jpg"
   }
 ];
